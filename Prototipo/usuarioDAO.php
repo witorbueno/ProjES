15956f5dff70263911f22ac5c02da8fed5baea66
witorbueno <49721429+witorbueno@users.noreply.github.com>
2019-06-06 09:38:58 -0300

    GRUD completo

--- Prototipo/usuarioDAO.php
+++ Prototipo/usuarioDAO.php
@@ -33,5 +33,25 @@
 			}
 			echo "<br>"; echo "Alterado com Sucesso";
 		}
+		function excluir($Usuario, $link){
+				$query = "DELETE FROM `usuario` WHERE Apelido = '".$Usuario->getApelido()."'";
+			echo $query; echo "<br>";
+	
+			if (!mysqli_query($link,$query)) {
+				die ("Não foi possível salvar".mysqli_error($link));
+			}
+			echo "Salvo com Sucesso";
+			
+		}
+			function consultar($Usuario, $link){
+			$query = "SELECT * FROM `usuario` WHERE Apelido = '".$Usuario->getApelido()."'";
+			$result = mysqli_query($link,$query);
+			if (!$result) {
+				echo ("Não foi possível encontrar no banco de dados./n Erro no MySQL: ".mysqli_error($link));
+				exit;
+			}
+			return mysqli_fetch_assoc($result);
+		}
+			
 	}	
 	?>
